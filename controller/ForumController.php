0c8e3cffec7c82514686f5a319344b51efd17cba
Benjamin Louis <louisbenetval@orange.fr>
2023-07-10 01:13:58 +0200

    MAJ

--- controller/ForumController.php
+++ controller/ForumController.php
@@ -288,7 +288,25 @@
             ];
         }
 
+        public function delReponse($id){                 // Fonction pour supprimer un Post
 
+            $reponseManager = new ReponseManager();
+            $postManager = new PostManager();
+
+            return [                                  // Le nom de la fonction doit correspondre avec le fichier cible pour accéder à celui ci
+
+                "view" => VIEW_DIR."forum/detailPost.php", // ATTENTION Gérer le retour vers la même page
+
+                "data" => [$reponseManager->delete($id),
+                "posts" => $postManager->findAll(),
+                "reponses" => (
+                    isset($id)
+                    ? $reponseManager->findListByIdDep($id, "Post", ["dateCreate", "DESC"]) // Pour afficher les réponses de chaque posts
+                    : $reponseManager->findAll(["dateCreate", "DESC"]) // Pour afficher toute les réponses
+                )
+                ]
+            ];
+        }
 
 
 
