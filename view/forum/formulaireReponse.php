53a20d4424495b8d03102c3fbddbd47b7828bb73
Benjamin Louis <louisbenetval@orange.fr>
2023-07-09 02:40:35 +0200

    MAJ

--- view/forum/formulaireReponse.php
+++ view/forum/formulaireReponse.php
@@ -33,4 +33,3 @@
 
 <a class="retour" href="index.php?ctrl=forum&action=listTopics&id=1">- TOPICS CUISINE -</a>
 <a class="retour" href="index.php?ctrl=forum&action=listTopics&id=2">- TOPICS PATISSERIE -</a>
-<a class="retour" href="index.php?ctrl=forum&action=detailPosts&id=1">- RETOUR -</a>
