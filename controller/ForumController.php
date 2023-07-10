7767e76d67de773f117e3a754cf7c35d812a8581
Benjamin Louis <louisbenetval@orange.fr>
2023-07-10 10:52:31 +0200

    MAJ

--- controller/ForumController.php
+++ controller/ForumController.php
@@ -84,12 +84,12 @@
             $name = filter_input(INPUT_POST, 'name', FILTER_SANITIZE_FULL_SPECIAL_CHARS);
         
             //$categoryManager->majCategory([':name' => $name, ':id' => $id]);   // Pour effectuer l'action de modification 
-            
+
             $categoryManager->majCategory($name, $id);   // Pour effectuer l'action de modification 
 
             return [                                    // Le nom de la fonction doit correspondre avec le fichier cible pour accéder à celui ci
                                            
-                //"view" => VIEW_DIR."forum/listCategories.php",
+                "view" => VIEW_DIR."forum/listCategories.php",
 
                 //"data" => ["categories" => $categoryManager->findAll(["name", "ASC"])]
 
