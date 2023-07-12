a62a6e94852bb64181a604bd71f977dcb34e2223
Benjamin Louis <louisbenetval@orange.fr>
2023-07-13 00:05:16 +0200

    MAJ

--- view/security/login.php
+++ view/security/login.php
@@ -11,7 +11,7 @@
 
     <h2>CONNEXION</h2>
 
-    <form action="index.php?ctrl=security&action=addUser" method="post">
+    <form action="index.php?ctrl=security&action=login" method="post">
        
         <label for="mail">Mail</label>
         <input type="email" name="mail" id="mail" required><br>
@@ -19,7 +19,7 @@
         <label for="pass1">Mot de passe</label>
         <input type="password" name="pass1" id="pass1" minlength="8" required><br>
 
-        <input id="submit" type="submit" name="addUser" value="CONNEXION">
+        <input id="submit" type="submit" name="login" value="CONNEXION">
     </form>
 
     </div>
