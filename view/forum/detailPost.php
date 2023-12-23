480b1e96b8c79a06aeec6688f8e9c7ab9590dccb
Benjamin Louis <louisbenetval@orange.fr>
2023-12-23 15:33:48 +0100

    MAJ

--- view/forum/detailPost.php
+++ view/forum/detailPost.php
@@ -4,81 +4,120 @@
 ?>
 
 <h2>DETAIL</h2>
-
-<div class="topicP">
-   
-    <table>
-
-        <?php
-            foreach($posts as $post){// On fait un foreach pour permettre l'affichage du tableau contenant les réponses 
-                $post->getText();   
-            }
-
-            // Afficher le message du Post sélectionné
-            echo "<div class='titreP'>".$post->getText()."</div>";
-            ?>
-
-            <!-- Lien pour répondre au Post -->
-            <a href="index.php?ctrl=forum&action=formulaireReponse&id=<?=$post->getId()?>">Répondre</a>
-            <?php  
-        ?>
-
-    </table> 
-
-</div>
-
 <?php
-    //$reponses = $result["data"]['reponses'];// Cette variable permet d'accéder et d'afficher les informations demandées dans cette page
-?>
+    if($reponses == true){// Créer une condition pour afficher un post sans réponses si il y a une ou plusieurs réponses alors afficher ci desous)
+?>  
 
-<div class="reponses">
+    <div class="topicP">
+    
+        <table>
 
-    <?php // Afficher les réponses sous forme de cartes à retourner
+            <?php
 
-        foreach($reponses as $reponse){
+                foreach($posts as $post){// On fait un foreach pour permettre l'affichage du tableau contenant les réponses 
+                    $post->getText();   
+                }
 
+                // Afficher le message du Post sélectionné
+                echo "<div class='titreP'>".$post->getText()."</div>";
+                ?>
+
+                <!-- Lien pour répondre au Post -->
+                <a href="index.php?ctrl=forum&action=formulaireReponse&id=<?=$post->getId()?>">Répondre</a>
+                <?php  
+            
             ?>
-                <div class="card-container">
-                    <div class="card">
-                        <div class="card-front">
-                            <p><?=$reponse->getUser()->getPseudo()?></p>
-                            <h3><?=$reponse->getDateCreate()?></h3>
-                        </div>
 
-                        <div class="card-back">
+        </table> 
 
-                            <h3>Réponse de <?=$reponse->getUser()->getPseudo()?></h3>
-                            <p><?=$reponse->getText()?></p>
+    </div>
 
-                            <div class="optionsR">
+    <div class="reponses">
 
-                                <!-- Pour supprimer la réponse sélectionnée directement dans la liste -->
-                                <!--<form action="index.php?ctrl=forum&action=delReponse&id=
-                                <?//=$reponse->getId()?>
-                                " method="post">-->
+        <?php // Afficher les réponses sous forme de cartes à retourner
 
-                                    <!-- Mettre une icône dans l'input -->
-                                    <!--<input type="image" class="suppR" alt="Supprimer" src="./public/img/supp.jpg">-->
+            foreach($reponses as $reponse){
 
-                                </form>
-                            
-                                <!-- Pour modifier une réponse -->
-                                <!--<form action="iindex.php?ctrl=forum&action=formulaireReponse&id=
-                                <?//=$reponse->getId()?>
-                                " method="post">-->
-                                    <!--<input type="image" class="majR" alt="Modifier" src="./public/img/maj.jpg">-->
-                                </form>
-
+                ?>
+                    <div class="card-container">
+                        <div class="card">
+                            <div class="card-front">
+                                <p><?=$reponse->getUser()->getPseudo()?></p>
+                                <h3><?=$reponse->getDateCreate()?></h3>
                             </div>
 
+                            <div class="card-back">
+
+                                <h3>Réponse de <?=$reponse->getUser()->getPseudo()?></h3>
+                                <p><?=$reponse->getText()?></p>
+
+                                <div class="optionsR">
+
+                                    <!-- Pour supprimer la réponse sélectionnée directement dans la liste -->
+                                    <!--<form action="index.php?ctrl=forum&action=delReponse&id=
+                                    <?//=$reponse->getId()?>
+                                    " method="post">-->
+
+                                        <!-- Mettre une icône dans l'input -->
+                                        <!--<input type="image" class="suppR" alt="Supprimer" src="./public/img/supp.jpg">-->
+
+                                    </form>
+                                
+                                    <!-- Pour modifier une réponse -->
+                                    <!--<form action="iindex.php?ctrl=forum&action=formulaireReponse&id=
+                                    <?//=$reponse->getId()?>
+                                    " method="post">-->
+                                        <!--<input type="image" class="majR" alt="Modifier" src="./public/img/maj.jpg">-->
+                                    </form>
+
+                                </div>
+
+                            </div>
                         </div>
                     </div>
-                </div>
-            <?php
-        }
-    ?>
+                <?php
+            }
 
-</div>
+
+        ?>
+
+    </div>
+
+    <?php
+
+}else{// Sinon afficher ci dessous (Page Liste Posts sans Réponses)
+
+    ?>
+    <div class="topicP">
+    
+        <table>
+
+            <?php
+
+                foreach($posts as $post){// On fait un foreach pour permettre l'affichage du tableau contenant les réponses 
+                    $post->getText();   
+                }
+
+                // Afficher le message du Post sélectionné
+                echo "<div class='titreP'>".$post->getText()."</div>";
+                ?>
+
+                <!-- Lien pour répondre au Post -->
+                <a href="index.php?ctrl=forum&action=formulaireReponse&id=<?=$post->getId()?>">Répondre</a>
+                <?php  
+            
+            ?>
+
+        </table> 
+
+    </div>
+    
+    <?php
+
+    echo "Il n'y a pas encore de réponses pour ce post";
+}
+
+?>
 
 <!-- Image en arrière plan -->
 <figure>
