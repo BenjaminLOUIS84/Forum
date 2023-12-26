d6ec28c39698255f5a3a8960b9dfca18e9ce6729
Benjamin Louis <louisbenetval@orange.fr>
2023-12-26 12:26:30 +0100

    MAJ

--- view/forum/formulaireReponse.php
+++ view/forum/formulaireReponse.php
@@ -1,3 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        <!-- <script src="https://cdn.tiny.cloud/1/zg3mwraazn1b2ezih16je1tc6z7gwp5yd4pod06ae5uai8pa/tinymce/5/tinymce.min.js" referrerpolicy="origin"></script> -->
+        <!-- <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.13.0/css/all.min.css" integrity="sha256-h20CPZ0QyXlBuAw7A+KluUYx/3pK+c7lYEpqLTlxjYQ=" crossorigin="anonymous" /> -->
+        
+        <!-- <link rel="stylesheet" href="<?= PUBLIC_DIR ?>/css/style.css"> -->
+        <link rel="stylesheet" href="public/css/style.css">
+        
+        <title>Réponse</title>
+    </head>
 <?php
 
     $reponses = $result["data"]['reponses'];
@@ -57,3 +71,4 @@
 </figure>
 
 <a class="retour" href="index.php?ctrl=forum&action=detailPost&id=<?=$reponse->getPost()->getId()?><?=$post->getText()?>">RETOUR</a>
+</html>
