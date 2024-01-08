9e840022cab12ca5d2be64c66d4eb84e40f1dbfd
Benjamin Louis <louisbenetval@orange.fr>
2024-01-08 12:09:34 +0100

    MAJ

--- view/layout.php
+++ view/layout.php
@@ -89,10 +89,10 @@
 
                     <a href="https://www.youtube.com/@lesrecettesdebenjamin">Créer par Benjamin LOUIS</a>
                     
-                    <div class="infos">
-                        <a href="index.php?ctrl=security&action=cgu">CGU</a>
-                        <a href="index.php?ctrl=security&action=mentions">Mentions légales</a>
-                    </div>
+                   
+                    <a href="index.php?ctrl=security&action=cgu">CGU</a> - 
+                    <a href="index.php?ctrl=security&action=mentions">Mentions légales</a>
+                
                 </div>
                 <!--<button id="ajaxbtn">Surprise en Ajax !</button> -> cliqué <span id="nbajax">0</span> fois-->
             </footer>
